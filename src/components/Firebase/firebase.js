5c29c573fa2ee19e038c037e01421e500b7923ed
Ivan Kuzmuk <divertechnology@gmail.com>
2018-12-03 19:14:23 -0300

    change permissions

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,33 +1,12 @@
 import app from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
-//import firebaseConfig from '../../firebase.config';
-
-const prodConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-};
-
-const devConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-};
-
-//const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
-
+import firebaseConfig from '../../firebase.config';
 
 class Firebase {
   constructor() {
-    
-    app.initializeApp(devConfig);
+    app.initializeApp(firebaseConfig);
+   // app.initializeApp(config);
     this.emailAuthProvider = app.auth.EmailAuthProvider;
     this.auth = app.auth();
     this.db = app.database();
